b0a5228bb370dd7a6870bb5dcdef27e6e4d895d1
kartheeswaran <kartheeswaranm@smackcoders.com>
2025-01-25 20:11:07 +0530

    main

--- resources/views/base/footer.blade.php
+++ resources/views/base/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- This is a footer -->
 <footer class="bg-light py-3 mt-4">
     <div class="container text-center">
-        <p class="mb-0">© 2025 Your Blog Name. All rights reserved.</p>
+        <p class="mb-0">© 2025 All rights reserved.</p>
         <p class="mb-0"><a href="#">Privacy Policy</a> | <a href="#">Terms of Service</a></p>
     </div>
 </footer>
